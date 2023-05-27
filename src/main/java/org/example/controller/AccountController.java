bc8c691ac91722fea6d9c2cf3f285ff4a1a29b8b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-27 14:33:08 +0200

    creating frames for the application

---
+++ src/main/java/org/example/controller/AccountController.java
@@ -0,0 +1,10 @@
+package org.example.controller;
+
+public class AccountController {
+
+    public void registerUser(){
+
+    }
+
+
+}
---
+++ src/main/java/org/example/model/Account.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+public class Account {
+
+private String firstName;
+private String secondName;
+private String lastName;
+private int pesel;
+private int phoneNumber;
+private String email;
+private String address;
+
+
+
+
+
+}
