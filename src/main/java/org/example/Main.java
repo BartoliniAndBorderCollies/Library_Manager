6c2a2d3aa088e8546c5642b9b1b6558e2c368a1d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-27 18:39:19 +0200

    connection trial with database successful after some changes.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.database.HibernateUtil;
+import org.hibernate.Session;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        // Check database version (because there is nothing else in database)
+        String sql = "select version()";
+
+        String result = (String) session.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+        session.close();
+
+        HibernateUtil.shutdown();
     }
+
 }
--- src/main/java/org/example/model/Account.java
+++ src/main/java/org/example/model/Account.java
@@ -1,17 +1,35 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String secondName;
+    @Column
+    private String lastName;
+    @Column
+    private int pesel;
+    @Column
+    private int phoneNumber;
+    @Column
+    private String email;
+    @Column
+    private String address;
 
-private String firstName;
-private String secondName;
-private String lastName;
-private int pesel;
-private int phoneNumber;
-private String email;
-private String address;
+    public Long getId() {
+        return id;
+    }
 
-
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 
 }
