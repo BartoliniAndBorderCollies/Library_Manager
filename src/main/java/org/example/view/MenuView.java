7ab9dddc80eb069948f209f034fb9378360dacea
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-28 10:20:42 +0200

    add View methods for messages to user

--- src/main/java/org/example/view/MenuView.java
+++
@@ -1,8 +0,0 @@
-package org.example.view;
-
-import org.example.command.MenuCommand;
-
-public class MenuView {
-
-
-}
---
+++ src/main/java/org/example/view/View.java
@@ -0,0 +1,32 @@
+package org.example.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class View {
+
+    private final List<String> lines = new ArrayList<>();
+
+    public void addLine(String text) {
+        lines.add(text);
+    }
+
+    public void clearLines() {
+        lines.clear();
+    }
+
+    public void print() {
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    public void update(String text) {
+        addLine(text);
+        print();
+        clearLines();
+    }
+
+
+
+}
