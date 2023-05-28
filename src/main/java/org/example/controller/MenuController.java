a2af8648f987d9cd253cecebaeb77237b66706a4
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-28 21:15:07 +0200

    add run() and welcome message plus askForInput() in MenuController

--- src/main/java/org/example/controller/MenuController.java
+++ src/main/java/org/example/controller/MenuController.java
@@ -1,18 +1,52 @@
 package org.example.controller;
 
-import org.example.model.Menu;
-import org.example.view.MenuView;
+import org.example.command.CreateAccount;
+import org.example.view.View;
+
+import java.util.Scanner;
 
 public class MenuController {
 
-    private final Menu menu;
+    private static final String WELCOME = """
+            Welcome to Library Manager application.
+            
+            Type a command. For help type "help"
+            """;
+    private static final String USER_COMMAND = "Command:";
+    private final View view;
+    private final CreateAccount createAccount;
 
-    private final MenuView menuView;
 
-    public MenuController(Menu menu, MenuView menuView) {
-        this.menu = menu;
-        this.menuView = menuView;
+    public MenuController(View view, CreateAccount createAccount) {
+        this.view = view;
+        this.createAccount = createAccount;
     }
+
+    public void run() {
+        view.update(WELCOME);
+        String userInput = askForInput(USER_COMMAND);
+
+        if(userInput.equalsIgnoreCase("create account")) {
+            createAccount.execute();
+        }
+    }
+
+    public String askForInput(String userInput) {
+        Scanner scanner = new Scanner(System.in);
+
+        view.update(userInput);
+        return scanner.nextLine();
+    }
+
+
+    private void showListOfCommands(String input) {
+
+
+
+    }
+
+
+
 
 
 }
